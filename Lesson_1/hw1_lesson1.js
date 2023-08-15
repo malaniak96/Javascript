2dc2b8312a0fd3a1d665db5891916cb9237c02ed
OlgaM <139258143+malaniak96@users.noreply.github.com>
2023-08-16 01:37:47 +0200

    lesson1 hw1

---
+++ Lesson_1/hw1_lesson1.js
@@ -0,0 +1,65 @@
+// hw part 1
+let a1= 'hello';
+console.log(a1);
+
+let a2= 'owu';
+console.log(a2);
+
+let a3= 'com';
+console.log(a3);
+
+let a4= 'ua';
+console.log(a4);
+
+let a5= 1;
+console.log(a5);
+
+let a6= 10;
+console.log(a6);
+
+let a7= -999;
+console.log(a7);
+
+let a8= 123;
+console.log(a8);
+
+let a9= 3.14;
+console.log(a9);
+
+let a10= 2.7;
+console.log(a10);
+
+let a11= 16;
+console.log(a11);
+
+let a12=true;
+console.log(a12);
+
+let a13=false;
+console.log(a13);
+
+let bRes= a5>a7;
+console.log(bRes);  // practice
+
+
+
+// hw part 2
+let fn = 'Olga';
+let mn = 'Jenny';
+let ln = 'Malaniak';
+
+let result = `${fn} ${mn} ${ln}`;
+console.log(result);
+
+
+// hw part 3
+let a= 100;
+let b= '100';
+let c= true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+
+// hw part 4
