fe3ca473b2b5fca64c824cd089504d2023362a9c
OlgaM <139258143+malaniak96@users.noreply.github.com>
2023-08-16 00:32:42 +0200

    lesson1

---
+++ Lesson_1/main.js
@@ -0,0 +1,44 @@
+let a1 = 'hello okten';
+console.log(a1);
+
+let b1 = 123;
+console.log(b1);
+
+const c1 = 'sdfjfnjfdj';
+
+// var v1 = 'hello';
+
+{
+    let a =  'asdf';
+
+
+}
+
+console.log(+'123');
+
+
+
+  let nameUser =  prompt('enter your name please');
+  console.log(nameUser);
+  if (!!nameUser){
+      alert('enter your name blin');
+  }
+
+
+  let arr = [111, 'okten', true];
+
+  console.log(arr);
+  console.log(arr[1]);
+  console.log(arr[0]);
+
+
+  let user = {
+      name: 'olga',
+      age: 13,
+      status: true
+  };
+
+console.log(user);
+console.log(user.name)
+console.log(user.age)
+console.log(user.status);
