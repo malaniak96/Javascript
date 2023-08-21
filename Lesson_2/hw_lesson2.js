269bdeb1c6b2d85ced5265ee00f46a6cb36b07ad
OlgaM <139258143+malaniak96@users.noreply.github.com>
2023-08-21 13:57:11 +0200

    lesson 2 hw part 3 & 4 completed

--- Lesson_2/hw_lesson2.js
+++ Lesson_2/hw_lesson2.js
@@ -1,4 +1,5 @@
-// hw part 1 Масиви та об'єкти:
+// HW Part 1 Масиви та об'єкти:
+
 //- Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
 let cars = [
     "bmw",
@@ -14,7 +15,7 @@ let cars = [
 ];
 console.log(cars);
 
-////Створити 3 об'єкти які описують книги.
+//Створити 3 об'єкти які описують книги.
 let book1 = {
     title: 'The Great Gatsby',
     pagecount: 340,
@@ -76,21 +77,167 @@ console.log(user[9].password);
 
 
 
-// /Логічні розгалуження:
+// HW Part 2 Логічні розгалуження:
 
 //- Є змінна х, якій ви надаєте довільне числове значення.
 // Якщо змінна x не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3
-let x= +prompt('number 6');
-if (x!=0) {
-    console.log('Correct');
-}
-else {
+let number= +prompt('pick a number');
+if (number === 0) {
     console.log('Incorrect');
 }
+else {
+    console.log('Correct');
+}
+
+//- Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число (в першу, другу, третю или четверту частину години).
+
+let time = prompt('minutes from 0-59 ?');
+if (time >= 0 && time <= 14) {
+    console.log( 'quarter 1');
+}
+else if (time >= 15 && time <= 29){
+    console.log('quarter 2');
+}
+else if (time >= 30 && time <= 44){
+    console.log('quarter 3');
+}
+else if (time >= 45 && time <= 59){
+    console.log('quarter 4');
+}
 
 
-//- Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
-// (в першу, другу, третю или четверту частину години).
+//- У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
+
+let day = prompt('pick a day of the month from 1 to 31');
+if (day >= 1 && time <= 10) {
+    console.log( 'decade 1');
+}
+else if (day >= 11 && day <= 20) {
+    console.log('decade 2');
+}
+else if (day >= 21 && day <= 31) {
+    console.log('decade 3');
+}
+
+//- Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день (можна замість плану на день, назву дня англійською).
+// - Користувач вводить або має два числа.
+// Потрібно знайти та вивести максимальне число з тих двох .
+// Також потрібно врахувати коли введені рівні числа.
+
+let schedule= +prompt('pick a day (1-7) of the week to see your schedule');
+switch (schedule) {
+    case 1:
+        console.log( '1. 4:30am – Wake up 2. Eat Breakfast  3. Exercise 4. Pack Lunch 5. Mid Afternoon Meditation 6. Top Off Groceries after Work 7. Clean Everything as soon as I get home! 8. Shower 9. Relax for the rest of the night');
+        break;
+    case 2:
+        console.log( '1. 4:30am – Wake up 2. Eat Breakfast  3. Exercise 4. Read book 5. Eat lunch 6. Cook dinner 7. Clean Everything as soon as I get home! 8. Shower 9. Watch a movie');
+        break;
+    case 3:
+        console.log( '1. 4:30am – Wake up 2. Eat Breakfast  3. Exercise 4. go shopping  5. Eat lunch 6. Cook dinner 7. go for a walk! 8. Shower 9. read a book');
+        break;
+    case 4:
+        console.log( '1. 4:30am – Wake up 2. Eat Breakfast  3. Exercise 4.cook lunch 5. Eat lunch 6. buy groceries 7.Shower 8. relax');
+        break;
+    case 5:
+        console.log( '1. 4:30am – Wake up 2. Eat Breakfast  3. Exercise 4. meditate 5. Eat lunch 6. Cook dinner 7. Shower 8. go out with friends to the bars');
+        break;
+    case 6:
+        console.log( '1. 4:30am – Wake up 2. Eat Breakfast  3. Exercise 4. clean the house 5. Eat lunch 6. get dressed/shower  9. go to dinner with family');
+        break;
+    case 7:
+        console.log( '1. 4:30am – Wake up 2. Eat Breakfast  3. Exercise 4. meditate. Eat lunch 6. relax the rest of the day ');
+        break;
+    default:
+        console.log('empty');
+}
+
+//- Користувач вводить або має два числа.
+// Потрібно знайти та вивести максимальне число з тих двох .
+// Також потрібно врахувати коли введені рівні числа.
+
+let f = 24;
+let d  =  23;
+
+if (f===d){
+    console.log('equal');
+}
+else if (f>d) {
+    console.log(f);
+}
+else {
+    console.log(d);
+}
+
+let r = 22;
+let k  =  23;
+
+if (r>k) {
+    console.log(r);
+}
+else if (r<k) {
+    console.log(k);
+}
+ else if (r===k){
+    console.log('equal');
+}
 
 
-let time =
+
+
+//HW Part 3
+//- є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
+// за допомоги  оператора || буде присвоювати змінній х значення "default"  якщо значення змінної х являється falsy (хибноподібні, тобто приводиться до false)
+
+let x = prompt('x') || 'default';
+console.log(x);
+
+
+//- з файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray. За допомоги іф перевірити кожен його елемент на тривалість навчання. У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+if (coursesAndDurationArray[0].monthDuration > 5){
+    console.log('super');
+}
+else  {
+    console.log('not good')
+}
+
+if (coursesAndDurationArray[1].monthDuration > 5){
+    console.log('super');
+}
+else  {
+    console.log('not good')
+}
+if (coursesAndDurationArray[2].monthDuration > 5){
+    console.log('super');
+}
+else  {
+    console.log('not good')
+}
+if (coursesAndDurationArray[3].monthDuration > 5){
+    console.log('super');
+}
+else  {
+    console.log('not good')
+}
+
+if (coursesAndDurationArray[4].monthDuration > 5){
+    console.log('super');
+}
+else  {
+    console.log('not good')
+}
+
+if (coursesAndDurationArray[5].monthDuration > 5){
+    console.log('super');
+}
+else  {
+    console.log('not good')
+}
